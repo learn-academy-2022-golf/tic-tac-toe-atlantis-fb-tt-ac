bf1cee431645a135604dc72b7ba38b0b1cf726d0
annabellachan <annabellac24@gmail.com>
2022-11-14 14:46:46 -0800

    story 3 completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [counter, setCounter] = useState(1)
   
   const handleGamePlay = (index) => {
     let updatedSquares = [...squares]
-    updatedSquares[index] = "🌴"
-    setSquares(updatedSquares)
+    
+    if(counter % 2 === 0) {
+      updatedSquares[index] = "🐢"
+      setSquares(updatedSquares)
+      setCounter(counter + 1)
+    } else {
+      updatedSquares[index] = "🌴"
+      setSquares(updatedSquares)
+      setCounter(counter + 1)
   }
+}
+
 
   return (
     <>
