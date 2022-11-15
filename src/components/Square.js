6c1bb9fe43a79b98e806ebc793a01e5a2a7c6340
annabellachan <annabellac24@gmail.com>
2022-11-15 10:44:30 -0800

    finished story 4

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,8 @@ import React from 'react'
 const Square = (props) => {
   const handleClick = () => {
     // alert(props.index)
-    props.handleGamePlay(props.index)
+    props.handleGamePlay(props.index, props.value)
+  
   }
   return (
     <div className="square" onClick={handleClick}>{props.value}</div>
